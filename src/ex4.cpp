d5dfdc884198da9dd90ebfdd3900843a37786020
Wilburn Ernesto <@wilburne>
2026-04-21 20:20:10 +0500

    Task 4 is done

---
+++ src/ex4.cpp
@@ -0,0 +1,90 @@
+#include "Arduino.h"
+
+#define LIGHT_SENSOR_PIN 33
+#define BLUE 14
+#define GREEN 27
+#define YELLOW 12
+#define RED 26
+
+bool B = true;
+bool G = true;
+bool Y = true;
+bool R = true;
+
+void setup(void) 
+{
+    Serial.begin(9600);
+    pinMode(BLUE, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(RED, OUTPUT);
+}
+
+void resetB(void);
+void reset(void);
+
+void loop(void) 
+{
+    int value = analogRead(LIGHT_SENSOR_PIN);
+
+    if (value >= 0 && value <= 1023)
+    {
+        reset();
+        digitalWrite(BLUE, HIGH);
+        if (B) 
+        {
+            Serial.println("band=BLUE");
+            resetB();
+            B = false;
+        }
+    } 
+    else if (value >= 1024 && value <= 2047)
+    {
+        reset();
+        digitalWrite(GREEN, HIGH);
+        if (G) 
+        {
+            Serial.println("band=GREEN");
+            resetB();
+            G = false;
+        }
+    } 
+    else if (value >= 2048 && value <= 3071)
+    {
+        reset();
+        digitalWrite(YELLOW, HIGH);
+        if (Y) 
+        {
+            Serial.println("band=YELLOW");
+            resetB();
+            Y = false;
+        }
+    } 
+    else if (value >= 3072 && value <= 4095)
+    {
+        reset();
+        digitalWrite(RED, HIGH);
+        if (R) 
+        {
+            Serial.println("band=RED");
+            resetB();
+            R = false;
+        }
+    }
+}
+
+void reset(void) 
+{
+    digitalWrite(BLUE, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+}
+
+void resetB(void) 
+{
+    B = true;
+    G = true;
+    Y = true;
+    R = true;
+}
